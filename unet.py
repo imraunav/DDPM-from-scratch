a62b4caa4d5492537df920fd5c15de1ddb918e0c
imraunav <raun.ghosh@gmail.com>
2024-03-02 01:06:50 +0530

    Fixed some code

--- unet.py
+++ unet.py
@@ -23,7 +23,7 @@ class DoubleConv(nn.Module):
             return self.double_conv(x)
 
 
-class DownBlock(nn.Module):
+class Down(nn.Module):
     def __init__(self, in_channels, out_channels, emb_dim=256):
         super().__init__()
         self.maxpool_conv = nn.Sequential(
@@ -43,7 +43,7 @@ class DownBlock(nn.Module):
         return x + emb
 
 
-class UpBlock(nn.Module):
+class Up(nn.Module):
     def __init__(self, in_channels, out_channels, emb_dim=256):
         super().__init__()
 
@@ -64,6 +64,31 @@ class UpBlock(nn.Module):
         x = self.conv(x)
         emb = self.emb_layer(t)[:, :, None, None].repeat(1, 1, x.shape[-2], x.shape[-1])
         return x + emb
+
+
+class SelfAttention(nn.Module):
+    def __init__(self, channels, size):
+        super(SelfAttention, self).__init__()
+        self.channels = channels
+        self.size = size
+        self.mha = nn.MultiheadAttention(channels, 4, batch_first=True)
+        self.ln = nn.LayerNorm([channels])
+        self.ff_self = nn.Sequential(
+            nn.LayerNorm([channels]),
+            nn.Linear(channels, channels),
+            nn.GELU(),
+            nn.Linear(channels, channels),
+        )
+
+    def forward(self, x):
+        x = x.view(-1, self.channels, self.size * self.size).swapaxes(1, 2)
+        x_ln = self.ln(x)
+        attention_value, _ = self.mha(x_ln, x_ln, x_ln)
+        attention_value = attention_value + x
+        attention_value = self.ff_self(attention_value) + attention_value
+        return attention_value.swapaxes(2, 1).view(
+            -1, self.channels, self.size, self.size
+        )
 
 
 class UNet(nn.Module):
