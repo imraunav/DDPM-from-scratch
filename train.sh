891609d0159c92044edd9d1a30eb84f91fdd503e
imraunav <raun.ghosh@gmail.com>
2024-03-03 01:04:44 +0530

    Changed GPU number

--- train.sh
+++ train.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-CUDA_VISIBLE_DEVICES=2 python -u train.py --batch_size 32 --learning_rate 0.0003 --dataset_path /storage/raunav/imagenet/ILSVRC/Data/CLS-LOC/train --max_epoch 1000  
+CUDA_VISIBLE_DEVICES=5 python -u train.py --batch_size 32 --learning_rate 0.0003 --dataset_path /storage/raunav/imagenet/ILSVRC/Data/CLS-LOC/train --max_epoch 1000  
