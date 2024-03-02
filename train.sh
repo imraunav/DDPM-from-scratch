f14ff6c34f11881bf93c1c189799085b6dfe6092
imraunav <raun.ghosh@gmail.com>
2024-03-02 22:35:13 +0530

    Set training dataset to DIV2K

--- train.sh
+++ train.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-CUDA_VISIBLE_DEVICES=6 python -u train.py --batch_size 64 --learning_rate 0.0003 --dataset_path /storage/public_datasets/imagenet/ILSVRC/Data/CLS-LOC/train --max_epoch 1000  
+CUDA_VISIBLE_DEVICES=6 python -u train.py --batch_size 64 --learning_rate 0.0003 --dataset_path /storage/public_datasets/DIV2K --max_epoch 1000  
