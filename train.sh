78f979b462f7516ee497bba41f31db05f2bfd806
imraunav <raun.ghosh@gmail.com>
2024-03-03 01:49:06 +0530

    batch size 32 -> 16

--- train.sh
+++ train.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 CUDA_VISIBLE_DEVICES=5 python -u train.py \
---batch_size 32 \
+--batch_size 16 \
 --learning_rate 0.0003 \
 --dataset_path /storage/raunav/imagenet/ILSVRC/Data/CLS-LOC/train \
 --max_updates 1000000  \
