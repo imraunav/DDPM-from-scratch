29c307895456092de54b97b9f150f774c094ae28
imraunav <raun.ghosh@gmail.com>
2024-03-07 23:32:46 +0530

    Scalling network

--- train_mnist.sh
+++ train_mnist.sh
@@ -3,4 +3,9 @@
 CUDA_VISIBLE_DEVICES=1 python -u train_mnist.py \
 --batch_size 32 \
 --learning_rate 0.0003 \
+--n_resblock 8 \
+--model_channel 16 \
+--groups 16 \
+--dropout 0.5 \
+--n_head 8 \
 --max_epoch 100  \
