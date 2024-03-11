ac671bfc147264b5689dd4655efb2615e8f1411a
imraunav <raun.ghosh@gmail.com>
2024-03-11 19:39:35 +0530

    Fixed line break in bash script

--- train.sh
+++ train.sh
@@ -4,6 +4,5 @@ CUDA_VISIBLE_DEVICES=1 python -u train.py \
 --batch_size 16 \
 --learning_rate 0.0003 \
 --image_size 256 \
-# --dataset_path /storage/raunav/imagenet/ILSVRC/Data/CLS-LOC/train \
 --dataset_path /storage/public_datasets/celeba_hq/CelebAMask-HQ/CelebA-HQ-img/ \
 --max_updates 100000  \
