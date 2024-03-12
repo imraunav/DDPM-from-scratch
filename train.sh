e545cf3c643ca076f71fa4e7602b272cecb46717
imraunav <raun.ghosh@gmail.com>
2024-03-12 21:39:10 +0530

    Reduced batch size, increased image size

--- train.sh
+++ train.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 CUDA_VISIBLE_DEVICES=4 python -u train.py \
---batch_size 32 \
+--batch_size 8 \
 --learning_rate 0.0003 \
---image_size 128 \
+--image_size 256 \
 --dataset_path /storage/public_datasets/celeba_hq/CelebAMask-HQ/CelebA-HQ-img/ \
 --max_epoch 100  \
