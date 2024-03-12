dfc36fd423b2929948a03725e10e7c0b0bc8b696
imraunav <raun.ghosh@gmail.com>
2024-03-12 22:12:34 +0530

    set batch size to 8

--- train.sh
+++ train.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 accelerate launch train.py \
---batch_size 16 \
+--batch_size 8 \
 --learning_rate 0.0003 \
 --image_size 256 \
 --dataset_path /storage/public_datasets/celeba_hq/CelebAMask-HQ/CelebA-HQ-img/ \
