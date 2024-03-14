99997960dee657b560c3cb02dea1bc3bbcdcbe9d
imraunav <raun.ghosh@gmail.com>
2024-03-14 11:21:11 +0530

    Trying deeper config

--- train.sh
+++ train.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 accelerate launch train.py \
---batch_size 8 \
---learning_rate 0.0003 \
+--batch_size 2 \
+--learning_rate 0.00002 \
 --image_size 256 \
 --dataset_path /storage/public_datasets/celeba_hq/CelebAMask-HQ/CelebA-HQ-img/ \
 --max_epoch 1000  \
