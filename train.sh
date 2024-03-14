f34f8430ae3de5d71cd812b83bb3cdc898512430
imraunav <raun.ghosh@gmail.com>
2024-03-14 11:47:52 +0530

    Increased batch size

--- train.sh
+++ train.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 accelerate launch train.py \
---batch_size 4 \
+--batch_size 8 \
 --learning_rate 0.00003 \
 --image_size 128 \
 --dataset_path /storage/public_datasets/celeba_hq/CelebAMask-HQ/CelebA-HQ-img/ \
